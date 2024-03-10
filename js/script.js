b3084e40d3373c8afebfe98ec3941c656a093629
fareshossam2 <fareshossam201@gmail.com>
2024-03-10 21:33:29 +0200

    new commit

--- js/script.js
+++ js/script.js
@@ -44,3 +44,36 @@ butt.innerHTML="Show";
 
 
 
+
+
+$(document).ready(function () {
+    $("#rev-content1").show();
+    $("#rev-content2").hide();         
+    $("#rev-content3").hide(); 
+    $("#s1").click(function() {
+        $("#rev-content1").fadeIn(4000);
+        $("#rev-content2").hide();
+        $("#rev-content3").hide();
+        $("#s1").css("background-color", " teal");
+        $("#s2").css("background-color", "aliceblue");
+        $("#s3").css("background-color", "aliceblue");
+    });
+        $("#s2").click(function() {
+            $("#rev-content2").fadeIn(4000);
+            $("#rev-content1").hide();
+            $("#rev-content3").hide();
+            $("#s2").css("background-color", "teal");
+            $("#s3").css("background-color", "aliceblue");
+            $("#s1").css("background-color", "aliceblue");
+        });
+            $("#s3").click(function() {
+                $("#rev-content3").fadeIn(4000);
+                $("#rev-content2").hide();
+                $("#rev-content1").hide();
+                $("#s3").css("background-color", "teal");
+                $("#s2").css("background-color", "aliceblue");
+                $("#s1").css("background-color", "aliceblue");
+            });
+       
+            
+    });
